ef876beef73c95f43a2f3ebd8026f2de9e71e5cf
affan ansari <affananxari839@gmail.com>
2024-08-28 17:29:34 +0500

    3.5

---
+++ python_variables/exercise_3.5.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+a = input("Enter a number:  ")
+b = input("Enter another number: ")
+product = float(a) * float(b)
+print("The product of " + a + " and " + b + " is " + str(product) + ".")
