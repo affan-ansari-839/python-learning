1214e59b9f376d3de1238e1d0cc146d94d596235
affan ansari <affananxari839@gmail.com>
2024-08-30 10:49:57 +0500

    data_structure

---
+++ python_data_structure/exercise_6.1.1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+#print the second item of the list
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+
+#print te last item of the list
+thislist = ["apple","bnana","cherry"]
+print(thislist[-1])
+
+#return the third, forth and fifth item
+thislist = ["apple","bnana","cherry","orange","kiwi","melon","mango"]
+print(thislist[2:5])
