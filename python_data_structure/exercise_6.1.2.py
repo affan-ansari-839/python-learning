14413140b0a3934cabcd87446bffd2cd26e9c2e6
affan ansari <affananxari839@gmail.com>
2024-08-30 11:21:48 +0500

    data_structure

---
+++ python_data_structure/exercise_6.1.2.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+#change the second value by replacing it with two new values
+thislist = ["apple","bnana","cherry"]
+thislist[1] = ["blackcurrant","watermelon"]
+print(thislist)
