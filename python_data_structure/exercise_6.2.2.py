8976f8b02724eeef684714da5899e79e839cd57d
affan ansari <affananxari839@gmail.com>
2024-09-02 14:31:53 +0500

    data_structure_tuple_sets_dictionary

---
+++ python_data_structure/exercise_6.2.2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+#this example returns the item from the beginning to, but not included, "kiwi
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[:4])
+
+# This example returns the items from index -4 (included) to index -1 (excluded)
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
+
---
+++ python_data_structure/exercise_6.2.3.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+# Join tuples
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+tuple3 = tuple1 + tuple2
+print(tuple3)
