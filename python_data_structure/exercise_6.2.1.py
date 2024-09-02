d070be0426bf5ba13982a8f454dae2cecf04a774
affan ansari <affananxari839@gmail.com>
2024-09-02 10:45:53 +0500

    data_structure_tuple

---
+++ python_data_structure/exercise_6.2.1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+#create a tuple
+thistuple = ("apple","banana","cheery")
+print(thistuple)
+
+#print the number of items in thisw tuple
+thistuple = ("apple","banana","cheery")
+len((thistuple))
+
+#one item tuple remember the comma ,
+thistuple = ("apple",)
+print(type(thistuple))
+
+#not a tuple
+thistuple = ("apple")
+print(type(thistuple))
+
+
+tuple1 = ("abc", 34, True, 40, "male")
+print(tuple1)
